626cf79d3c3b4ddcaeca27f492b56dc053bbc251
natuthe1634 <Princenatu22@gmail.com>
2023-04-20 21:38:20 -0700

    FIRST

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,45 @@
-
+// Your Code Here
+async function main() {
+    let response = await fetch("http://localhost:3001/listBooks", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    let retrieveList = await response.json();
+    console.log(retrieveList);
+    retrieveList.forEach(renderBook);
+  }
+  
+  function renderBook(book) {
+    let root = document.querySelector("#root");
+  
+    let li = document.createElement("li");
+    li.textContent = book.title;
+  
+    let quantityInput = document.createElement("input");
+    quantityInput.value = book.quantity;
+  
+    let saveButton = document.createElement("button");
+    saveButton.textContent = "Submit";
+  
+    saveButton.addEventListener("click", () => {
+      fetch("http://localhost:3001/updateBook", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id: book.id,
+          quantity: quantityInput.value,
+        }),
+      });
+    });
+  
+    li.append(quantityInput, saveButton);
+  
+    root.append(li);
+  }
+  
+  main();
+  
 // Your Code Here
